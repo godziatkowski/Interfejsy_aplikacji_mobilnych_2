be998d63bc371dcab773ac67722e65dc4a7d247e
Jan Godziątowski <ph.godzio@gmail.com>
2016-06-04 00:27:50 +0200

    Initialize project with angularJS
    *In defaultJS add logic responsible for resizing content part when window is resized
    *Add scroll when content is too long to fit window

---
+++ CurrencyViewerAppJS/js/app.config.js
@@ -0,0 +1,53 @@
+﻿(function () {
+    'use strict';
+
+    var config = {
+    };
+
+    angular
+            .module('currencyViewerApp')
+            .constant('config', config)
+            .run(['$rootScope', '$state',
+                function ($rootScope, $state) {
+                    $('#content').height($('#app').height() - 52);
+                    $state.go('home')
+                    $rootScope.$on('$stateChangeStart', function (event, toState,
+                            toStateParams) {
+                        $rootScope.toState = toState;
+                        $rootScope.toStateParams = toStateParams;                        
+
+                    });
+
+                    $rootScope.back = function () {
+                        if ($state.get($rootScope.previousStateName) === null) {
+                            $state.go('home');
+                        } else {
+                            $state.go($rootScope.previousStateName, $rootScope.previousStateParams);
+                        }
+                    };
+
+                }])
+            .config(configure);
+
+    configure.$inject = [
+        '$urlRouterProvider',
+        '$stateProvider',
+    ];
+
+    function configure($urlRouterProvider, $stateProvider) {
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider.state('root', {
+            abstract: true,
+            views: {
+                'content@': {
+                    template: '<ui-view />'
+                },
+                'navbar@': {
+                    templateUrl: 'views/navbar/navbar.html',
+                    controller: 'NavbarCtrl'
+                }
+            }            
+        });
+    }
+})();
