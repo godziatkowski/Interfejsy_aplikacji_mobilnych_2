16da8630cdeddd2966a7a2fbbdd1cd0e4f8ca696
Jan Godziątowski <ph.godzio@gmail.com>
2016-06-04 00:30:56 +0200

    Add view with list of currency files

---
+++ CurrencyViewerAppJS/js/fileList/fileList.controller.js
@@ -0,0 +1,52 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('currencyViewerApp')
+        .controller('FileListCtrl', FileListCtrl);
+
+    FileListCtrl.$inject = [
+        '$scope',
+        '$state',
+        'fallback',
+        'fileListDownloader'
+    ];
+
+    function FileListCtrl($scope, $state, fallback, fileListDownloader) {
+        $scope.years = fillYears();
+        $scope.year = moment().year();
+        $scope.files = [];
+        $scope.loadCurrencyFromFile = loadCurrencyFromFile;
+        $scope.loadFiles = loadFiles;
+        loadFiles();
+
+        function loadCurrencyFromFile(file) {
+            $state.go('home', { 'fileName': file.fileName });
+        }
+
+        function loadFiles(year) {
+            $scope.files = [];
+            $scope.loading = true;
+            if (year && year !== moment().year()) {
+                fileListDownloader.download('dir' + year).then(function (result) {
+                    $scope.files = result;
+                    $scope.loading = false;
+                });
+            } else {
+                fileListDownloader.download('dir').then(function (result) {
+                    $scope.files = result;
+                    $scope.loading = false;
+                });
+            }
+        }
+
+        function fillYears() {
+            var years = [];
+            var currentYear = moment().year();
+            for (var year = 2002; year <= currentYear; year++) {
+                years.push(year);
+            }
+            return years;
+        }
+
+    }
+})();
---
+++ CurrencyViewerAppJS/js/fileList/fileList.route.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+
+    angular
+            .module('currencyViewerApp')
+            .config(FileListRouteProvider);
+
+    FileListRouteProvider.$inject = ['$stateProvider'];
+
+    function FileListRouteProvider($stateProvider) {
+        $stateProvider.state('fileList', {
+            parent: 'root',
+            url: '/fileList',
+            templateUrl: 'views/fileList/fileList.html',
+            controller: 'FileListCtrl',
+            resolve: {
+                fallback: function () {
+                    return undefined;
+                }
+            }
+        });
+    }
+})();
