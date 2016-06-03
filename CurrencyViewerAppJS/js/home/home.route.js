018d6e203d6f3e8cf1c8da2b7e448a2298a42b87
Jan Godziątowski <ph.godzio@gmail.com>
2016-06-04 00:30:31 +0200

    Create home view which helds list of currencies

---
+++ CurrencyViewerAppJS/js/home/home.route.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+
+    angular
+            .module('currencyViewerApp')
+            .config(HomeRouteProvider);
+
+    HomeRouteProvider.$inject = ['$stateProvider'];
+
+    function HomeRouteProvider($stateProvider) {
+        $stateProvider.state('home', {
+            parent: 'root',
+            url: '/home?fileName',
+            templateUrl: 'views/home/home.html',
+            controller: 'HomeCtrl',
+            resolve: {
+                file: ['$stateParams', function ($stateParams) {
+                    return $stateParams.fileName;
+                }]
+            }
+        });
+    }
+})();
