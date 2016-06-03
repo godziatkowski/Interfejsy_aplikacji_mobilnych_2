ee488fdddf555af0ef7f88709a79c1a70413d22d
Jan Godziątowski <ph.godzio@gmail.com>
2016-06-04 00:30:09 +0200

    Create navbar view

---
+++ CurrencyViewerAppJS/js/navbar/navbar.controller.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('currencyViewerApp')
+        .controller('NavbarCtrl', NavbarCtrl);
+
+    NavbarCtrl.$inject = [
+        '$scope',
+        '$rootScope'
+    ];
+
+    function NavbarCtrl($scope, $rootScope) {
+        $scope.loadLast = loadLast;
+        $scope.exit = exit;
+
+        function loadLast() {
+            $rootScope.$broadcast('loadLastEvent');
+        }
+
+        function exit() {
+            window.close();
+        }
+    }
+})();
