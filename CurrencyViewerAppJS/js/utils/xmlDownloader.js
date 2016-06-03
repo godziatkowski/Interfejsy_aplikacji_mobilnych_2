a18a66a7105c3b96478d746eca5347b77e28eb8c
Jan Godziątowski <ph.godzio@gmail.com>
2016-06-04 00:28:34 +0200

    Create logic responsible for downloading and parsing xml files from NBP site

---
+++ CurrencyViewerAppJS/js/utils/xmlDownloader.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('currencyViewerApp')
+    .service('xmlDownloader', xmlDownloader);
+
+    xmlDownloader.$inject = ['$http'];
+
+    function xmlDownloader($http) {
+        var url = 'http://www.nbp.pl/kursy/xml/';
+
+        return {
+            download: load
+        }
+
+        function load(file) {
+            return $http({
+                method: 'GET',
+                url: url + file + ".xml",
+                headers: {
+                    'Content-Type': 'application/xml'
+                },
+                
+            }).then(function (result) {
+                var x2js = new X2JS();
+                var data = x2js.xml_str2json(result.data.substring(44));
+                for (var index = 0 ; index < data.tabela_kursow.pozycja.length; index++) {
+                    var average = Number(data.tabela_kursow.pozycja[index].kurs_sredni.replace(',','.'));
+                    data.tabela_kursow.pozycja[index].kurs_sredni = average.toFixed(2);
+                }
+                data.tabela_kursow.data_publikacji = moment(data.tabela_kursow.data_publikacji).format('dddd, MMMM Do YYYY')
+
+                return data.tabela_kursow;
+                
+            });
+
+        }
+
+    }
+
+})();
